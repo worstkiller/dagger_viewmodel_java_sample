efc105ed4b3bfae8fcb78077fde7d5bbf913cd49
vikas kumar <vkbookworm@gmail.com>
2019-04-02 20:18:55 +0530

    refactor: fixed the dagger base application

--- app/src/main/java/com/vikas/dagger/base/BaseApplication.java
+++ app/src/main/java/com/vikas/dagger/base/BaseApplication.java
@@ -1,25 +1,32 @@
 package com.vikas.dagger.base;
 
-import com.vikas.dagger.di.component.AppComponent;
+import android.app.Activity;
+import android.app.Application;
+
 import com.vikas.dagger.di.component.DaggerAppComponent;
 
+import javax.inject.Inject;
+
 import dagger.android.AndroidInjector;
-import dagger.android.support.DaggerApplication;
+import dagger.android.DispatchingAndroidInjector;
+import dagger.android.HasActivityInjector;
 
 /**
  * application level initialization for dagger components
  */
-public class BaseApplication extends DaggerApplication {
+public class BaseApplication extends Application implements HasActivityInjector {
+
+    @Inject
+    DispatchingAndroidInjector<Activity> dispatchingAndroidInjector;
 
     @Override
     public void onCreate() {
         super.onCreate();
+        DaggerAppComponent.builder().application(this).build().inject(this);
     }
 
     @Override
-    protected AndroidInjector<? extends DaggerApplication> applicationInjector() {
-        AppComponent component = DaggerAppComponent.builder().application(this).build();
-        component.inject(this);
-        return component;
+    public AndroidInjector<Activity> activityInjector() {
+        return dispatchingAndroidInjector;
     }
 }
